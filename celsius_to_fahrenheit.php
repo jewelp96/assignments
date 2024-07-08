c6dab8999907f668a5eed3632fa4a5312ce62878
jewel <eazybuy265@gmail.com>
2024-07-08 12:50:24 +0600

    assignments done

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,13 @@
+<?php
+// Celsius temperature
+$celsius = 32;
+
+// Convert Celsius to Fahrenheit
+$fahrenheit = ($celsius * 9/5) + 32;
+
+// Print the result
+echo $celsius . " degrees Celsius is equal to " . $fahrenheit . " degrees Fahrenheit.";
+$celsius1 = 89.6;
+printf ("%.2f",$celsius1);
+
+
